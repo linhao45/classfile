9cd379cf0507b93f1dc8eb4246e7f521b673929a
linhao45 <837367260@qq.com>
2020-09-28 19:40:14 +0800

    20200928-2

---
+++ static/js/playerconfig.js
@@ -0,0 +1,7 @@
+﻿var MacPlayerConfig={};
+//参数开始
+MacPlayerConfig={"width":"100%","height":"100%","widthmob":"100%","heightmob":"100%","widthpop":"0","heightpop":"600","second":"5","prestrain":"","buffer":"","parse":"","autofull":"0","showtop":"1","showlist":"1","flag":"0","colors":"000000,F6F6F6,F6F6F6,333333,666666,FFFFF,FF0000,2c2c2c,ffffff,a3a3a3,2c2c2c,adadad,adadad,48486c,fcfcfc"};
+//参数结束
+//缓存开始
+MacPlayerConfig.player_list={"alizy":{"show":"\u84dd\u5149\u63a8\u8350","des":"","ps":"0","parse":""},"weiyun":{"show":"\u84dd\u5149\u4e0d\u5361","des":"","ps":"0","parse":""},"qq":{"show":"\u84dd\u5149\u5907\u7528","des":"","ps":"0","parse":""},"ckm3u8":{"show":"\u5907\u7528\u7ebf\u8def","des":"","ps":"0","parse":""},"niuxyun":{"show":"\u6d4b\u8bd5\u64ad\u653e\u5668","des":"","ps":"0","parse":""},"mahua":{"show":"\u9ad8\u6e05\u5907\u7528\uff08\u6709\u5e7f\u544a\uff09","des":"","ps":"0","parse":""}},MacPlayerConfig.downer_list={"http":{"show":"http\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""},"xunlei":{"show":"xunlei\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""}},MacPlayerConfig.server_list={"server1":{"show":"\u6d4b\u8bd5\u670d\u52a1\u56681","des":"des\u63d0\u793a\u4fe1\u606f1"}};
+//缓存结束
